9ded3362139c739956b35dc6da69f98cf907aab9
Wick3r <danielrossner@web.de>
2015-03-10 16:11:23 +0100

    Implement an abstract crawler for easier usage

---
+++ src/main/java/artcitlocator/webcrawler/AbstractCrawler.java
@@ -0,0 +1,65 @@
+package artcitlocator.webcrawler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import artcitlocator.webcrawler.config.Configuration;
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+
+public abstract class AbstractCrawler extends WebCrawler {
+
+	private List<Pattern> pattern;
+	private String domain;
+	
+	public AbstractCrawler() {
+		super();
+		Configuration conf = new Configuration();
+		List<String> patternStrings = conf.getPatternByName(getCrawlerName());
+		pattern = new ArrayList<Pattern>();
+		for(int i = 0; i < patternStrings.size(); i++){
+			pattern.add(Pattern.compile(patternStrings.get(i)));
+		}
+		
+		domain = conf.getCrawlerByName(getCrawlerName()).getUrl().toLowerCase();
+	}
+	
+	public abstract String getCrawlerName();
+
+	@Override
+	public boolean shouldVisit(Page referringPage, WebURL url) {
+		String href = url.getURL().toLowerCase();
+		if(!href.startsWith(domain)) return false;
+		for(Pattern pat : pattern){
+			if(!pat.matcher(href).matches()) return false;
+		}
+		
+		return true;
+	}
+
+	/**
+	 * This function is called when a page is fetched and ready to be processed
+	 * by your program.
+	 */
+	@Override
+	public void visit(Page page) {
+		String url = page.getWebURL().getURL();
+		System.out.println("URL: " + url);
+
+		if (page.getParseData() instanceof HtmlParseData) {
+			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+			String text = htmlParseData.getText();
+			String html = htmlParseData.getHtml();
+			Set<WebURL> links = htmlParseData.getOutgoingUrls();
+
+			System.out.println("Text length: " + text.length());
+			System.out.println("Html length: " + html.length());
+			System.out.println("Number of outgoing links: " + links.size());
+		}
+	}
+
+}
--- src/main/java/artcitlocator/webcrawler/NYTCrawler.java
+++ src/main/java/artcitlocator/webcrawler/NYTCrawler.java
@@ -1,64 +1,20 @@
+/**
+ * 
+ */
 package artcitlocator.webcrawler;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.regex.Pattern;
+/**
+ * @author Daniel
+ *
+ */
+public class NYTCrawler extends AbstractCrawler {
 
-import artcitlocator.webcrawler.config.Configuration;
-import edu.uci.ics.crawler4j.crawler.Page;
-import edu.uci.ics.crawler4j.crawler.WebCrawler;
-import edu.uci.ics.crawler4j.parser.HtmlParseData;
-import edu.uci.ics.crawler4j.url.WebURL;
-
-public class NYTCrawler extends WebCrawler {
-
-	private final String CRAWLER_NAME = "NYT";
-	private List<Pattern> pattern;
-	private String domain;
-	
-	public NYTCrawler() {
-		super();
-		Configuration conf = new Configuration();
-		List<String> patternStrings = conf.getPatternByName(CRAWLER_NAME);
-		pattern = new ArrayList<Pattern>();
-		for(int i = 0; i < patternStrings.size(); i++){
-			pattern.add(Pattern.compile(patternStrings.get(i)));
-		}
-		
-		domain = conf.getCrawlerByName(CRAWLER_NAME).getUrl().toLowerCase();
-	}
-
-	@Override
-	public boolean shouldVisit(Page referringPage, WebURL url) {
-		String href = url.getURL().toLowerCase();
-		if(!href.startsWith(domain)) return false;
-		for(Pattern pat : pattern){
-			if(!pat.matcher(href).matches()) return false;
-		}
-		
-		return true;
-	}
-
-	/**
-	 * This function is called when a page is fetched and ready to be processed
-	 * by your program.
+	/* (non-Javadoc)
+	 * @see artcitlocator.webcrawler.AbstractCrawler#getCrawlerName()
 	 */
 	@Override
-	public void visit(Page page) {
-		String url = page.getWebURL().getURL();
-		System.out.println("URL: " + url);
-
-		if (page.getParseData() instanceof HtmlParseData) {
-			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-			String text = htmlParseData.getText();
-			String html = htmlParseData.getHtml();
-			Set<WebURL> links = htmlParseData.getOutgoingUrls();
-
-			System.out.println("Text length: " + text.length());
-			System.out.println("Html length: " + html.length());
-			System.out.println("Number of outgoing links: " + links.size());
-		}
+	public String getCrawlerName() {
+		return "NYT";
 	}
 
 }
